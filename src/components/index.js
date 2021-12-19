c7a2c06e865555d100013aafb2dc1e90fcf6f4f6
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-12-19 10:42:48 -0500

    perlin noise

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", init, false);
+
 import * as THREE from "three";
 import { GUI } from "dat.gui";
 
@@ -11,13 +13,11 @@ import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHel
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
-import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass.js";
+// import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass.js";
 // import {ShaderPass} from "three/examples/jsm/postprocessing/ShaderPass.js";
 // import {SSAOPass} from "three/examples/jsm/postprocessing/SSAOPass.js";
 
 // import {LuminosityShader} from "three/examples/jsm/shaders/LuminosityShader.js";
-
-window.addEventListener("load", init, false);
 
 /*
 	Init function
@@ -27,8 +27,9 @@ function init() {
 	createScene();
 	createLighting();
 	loadObjects();
-	// createPrimitive();
+	createPrimitive();
 	createGUI();
+
 	tick();
 }
 
@@ -37,7 +38,7 @@ function init() {
 	Scene + Camera + Renderer
 */
 const scene = new THREE.Scene();
-let camera, renderer, controls, importedModule, composer;
+let camera, renderer, controls, composer;
 let start = Date.now();
 let gui = new GUI();
 let isMobile;
@@ -93,7 +94,7 @@ function createScene() {
 			0.375
 		)
 	);
-	composer.addPass(new GlitchPass(64));
+	// composer.addPass(new GlitchPass(69));
 
 	renderer.shadowMap.enabled = true;
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -213,6 +214,63 @@ function loadObjects() {
 	});
 }
 
+var mat;
+var primitiveElement = function () {
+	this.mesh = new THREE.Object3D();
+	mat = new THREE.ShaderMaterial({
+		wireframe: false,
+		//fog: true,
+		uniforms: {
+			time: {
+				type: "f",
+				value: 0.0,
+			},
+			pointscale: {
+				type: "f",
+				value: 0.0,
+			},
+			decay: {
+				type: "f",
+				value: 0.0,
+			},
+			complex: {
+				type: "f",
+				value: 0.0,
+			},
+			waves: {
+				type: "f",
+				value: 0.0,
+			},
+			eqcolor: {
+				type: "f",
+				value: 0.0,
+			},
+			fragment: {
+				type: "i",
+				value: true,
+			},
+			redhell: {
+				type: "i",
+				value: true,
+			},
+		},
+		vertexShader: document.getElementById("vertexShader").textContent,
+		fragmentShader: document.getElementById("fragmentShader").textContent,
+	});
+	var geo = new THREE.IcosahedronBufferGeometry(3, 7);
+	var mesh = new THREE.Points(geo, mat);
+
+	//---
+	this.mesh.add(mesh);
+};
+
+var _primitive;
+function createPrimitive() {
+	_primitive = new primitiveElement();
+	// _primitive.mesh.scale.set(0.08, 0.08, 0.08);
+	scene.add(_primitive.mesh);
+}
+
 /**
  * Particles
  */
@@ -236,6 +294,25 @@ Array(300).fill().forEach(addStar);
 const clock = new THREE.Clock();
 function tick() {
 	// const elapsedTime = clock.getElapsedTime();
+
+	var performance = Date.now() * 0.003;
+
+	_primitive.mesh.rotation.y += options.perlin.vel;
+	_primitive.mesh.rotation.x =
+		(Math.sin(performance * options.spin.sinVel) *
+			options.spin.ampVel *
+			Math.PI) /
+		180;
+	//---
+	mat.uniforms["time"].value = options.perlin.speed * (Date.now() - start);
+	mat.uniforms["pointscale"].value = options.perlin.perlins;
+	mat.uniforms["decay"].value = options.perlin.decay;
+	mat.uniforms["complex"].value = options.perlin.complex;
+	mat.uniforms["waves"].value = options.perlin.waves;
+	mat.uniforms["eqcolor"].value = options.perlin.eqcolor;
+	mat.uniforms["fragment"].value = options.perlin.fragment;
+	mat.uniforms["redhell"].value = options.perlin.redhell;
+
 	if (material.displacementScale <= 30) {
 		material.displacementScale += 0.01;
 	} else {
@@ -271,10 +348,50 @@ function onWindowResize() {
 /**
  * Updates objects on each frame
  */
+
+var options = {
+	perlin: {
+		vel: 0.002,
+		speed: 0.0005,
+		perlins: 4.0,
+		decay: 0.1,
+		complex: 0.3,
+		waves: 20.0,
+		eqcolor: 11.0,
+		fragment: true,
+		redhell: true,
+	},
+	spin: {
+		sinVel: 0.0,
+		ampVel: 80.0,
+	},
+};
 function createGUI() {
 	const cameraFolder = gui.addFolder("Camera");
-	cameraFolder.add(camera.position, "x", 0, 60).step(0.001);
-	cameraFolder.add(camera.position, "y", 0, 60).step(0.001);
-	cameraFolder.add(camera.position, "z", 0, 60).step(0.001);
+	cameraFolder.add(camera.position, "x", 0.00, 60.00).step(0.001);
+	cameraFolder.add(camera.position, "y", 0.00, 60.00).step(0.001);
+	cameraFolder.add(camera.position, "z", 0.00, 60.00).step(0.001);
 	cameraFolder.open();
+
+	// var mathGUI = gui.addFolder("Math Options");
+	// mathGUI.add(options.spin, "sinVel", 0.0, 0.5).name("Sine").listen();
+	// mathGUI.add(options.spin, "ampVel", 0.0, 90.0).name("Amplitude").listen();
+	//mathGUI.open();
+
+	// var perlinGUI = gui.addFolder("Setup Perlin Noise");
+	// perlinGUI.add(options.perlin, "perlins", 1.0, 5.0).name("Size").step(1);
+	// perlinGUI.add(options.perlin, "speed", 0.0, 0.0005).name("Speed").listen();
+	// perlinGUI.add(options.perlin, "decay", 0.0, 1.0).name("Decay").listen();
+	// perlinGUI.add(options.perlin, "waves", 0.0, 20.0).name("Waves").listen();
+	// perlinGUI.add(options.perlin, "fragment", true).name("Fragment");
+	// perlinGUI.add(options.perlin, "complex", 0.1, 1.0).name("Complex").listen();
+	// perlinGUI.add(options.perlin, "redhell", true).name("Electroflow");
+	// perlinGUI.add(options.perlin, "eqcolor", 0.0, 15.0).name("Hue").listen();
+	// perlinGUI.open();
+
+	var primitivePosGUI = gui.addFolder("Primitive POS");
+	primitivePosGUI.add(_primitive.mesh.position, "x", 0.00, 20.00).step(0.001);
+	primitivePosGUI.add(_primitive.mesh.position, "y", 0.00, 20.00).step(0.001);
+	primitivePosGUI.add(_primitive.mesh.position, "z", 0.00, 20.00).step(0.001);
+	primitivePosGUI.open();
 }
